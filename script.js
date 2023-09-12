14105dbed3e5cef2ca7a5f9bafb5ece0f1036116
rajironman <128172464+rajironman@users.noreply.github.com>
2023-09-12 17:52:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ let projects = [
         'link':'https://rajironman.github.io/image-cropper',
         'skills':['HTML','CSS','Javascript'],
         'description':[
-            'point 1',
-            'point 2',
-            'point 3'
-        ],
-        'bg_color':'darkblue'
+
+            'Developed a JavaScript image cropper utilizing advanced event tracking techniques for seamless responsiveness.',
+            'Supported multiple image input methods: file selection, drag and drop, and image URL.',
+            'Utilized closure technique to prevent global scope pollution and ensure code encapsulation.'
+        
+                ],
+        'bg_color':'darkblue',
+        'show':'link'
     }
     ,
     {
@@ -16,12 +19,44 @@ let projects = [
         'link':'https://rajironman.github.io/indian-restaurant',
         'skills':['HTML','CSS','Javascript','Angular'],
         'description':[
-            'point 1',
-            'point 2',
-            'point 3'
+            'Leveraged Angular\'s RouterService to enable smooth navigation and create a single-page application.',
+            'Established efficient inter-component communication using Angular Services.'
         ],
-        'bg_color':'black'
-    }
+        'bg_color':'black',
+        'show':'link'
+    },
+    {
+        'title':'Advertising Website',
+        'link':'https://raj.epizy.com/ts2',
+        'skills':['HTML','CSS','Javascript','PHP','MySQL'],
+        'description':[
+            'Advertisement details are fetched from the server in JSON format using AJAX.',
+            'User credentials are hashed prior to storage in the database and also encrypted at the client side.',
+            'Resizing & optimizing the images to reduce file size upto 20kb - 35kb approx.'
+                ],
+        'bg_color':'black',
+        'show':'link'
+    },
+    {
+        'title':'Guessing Number Game',
+        'link':'https://rajironman.github.io/guessing-number-game',
+        'skills':['HTML','CSS','Javascript','Angular'],
+        'description':[
+            'Single Page Application is built with the help of Angular Components.',
+            '@ViewChild decorator and EventEmitter allow interaction between component hirearchy.'
+        ],
+        'bg_color':'black',
+        'show':'link'
+    },
+    {
+        'title':'Flight Delay Prediction',
+        'link':'https://github.com/rajironman/flight_delay_prediction',
+        'skills':['Python','Machine Learning','HTML','CSS','Javascript'],
+        'description':[
+            'Different metrics are used to compare three models trained using Random Forest Classifier , Decision Tree Classifier , ANN. '
+        ],
+        'bg_color':'black',
+    },
 ]
 
 
@@ -40,6 +75,7 @@ function addProject(project){
     let skills_p = document.createElement('p')
     let description_ul = document.createElement('ul')
     let iframe = document.createElement('iframe')
+    let image = document.createElement('img')
 
     title_h1.innerHTML = project['title']
     link_a.href = project.link
@@ -52,10 +88,16 @@ function addProject(project){
     };
 
     iframe.src = project.link
+    image.src = project.image
 
     project_details_div.append(title_h1,skills_p,description_ul,link_a)
 
-    let el = [iframe,project_details_div]
+    let el = [project_details_div]
+
+    if(project.show == 'link')
+    el.push(iframe)
+    if(project.show == 'image')
+    el.push(image)
 
     if(projects_section.childNodes.length % 2 == 0)
     el.reverse()
